725e64b4577b38d49e3de95f6c4c634aac2ba1b4
Daniel Wiberg <wibbe@blueeye.se>
2011-07-04 11:36:26 +0200

    Updated the simple example so it actually works.

--- examples/simple.js
+++ examples/simple.js
@@ -3,6 +3,14 @@ var webgl = require('../webgl');
 console.log("Initializing...")
 webgl.initialize();
 
+var gl = webgl.getContext();
+
+while (webgl.tick())
+{
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  
+  gl.swapBuffers();
+}
 
 
 webgl.destroy();
